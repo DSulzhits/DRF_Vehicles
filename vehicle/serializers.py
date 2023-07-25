2174ef19bd40e04183d90d009a6fdaaf8edb81d3
DSulzhits <dsulzhits@gmail.com>
2023-07-25 20:37:49 +0300

    create MotorcycleSerializer

--- vehicle/serializers.py
+++ vehicle/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from vehicle.models import Car
+from vehicle.models import Car, Motorcycle
 
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
+        fields = '__all__'
+
+
+class MotorcycleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Motorcycle
         fields = '__all__'
