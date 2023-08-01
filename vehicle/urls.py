662952450ce698c65b4367d8b8810e9ea9c03cec
DSulzhits <dsulzhits@gmail.com>
2023-08-01 12:42:13 +0300

    add Mileage urls

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -4,7 +4,7 @@ from vehicle.apps import VehicleConfig
 from rest_framework.routers import DefaultRouter
 
 from vehicle.views import CarViewSet, MotorcycleCreateAPIView, MotorcycleListAPIView, MotorcycleUpdateAPIView, \
-    MotorcycleDestroyAPIView
+    MotorcycleDestroyAPIView, MileageCreateAPIView
 
 app_name = VehicleConfig.name
 
@@ -12,9 +12,14 @@ router = DefaultRouter()
 router.register(prefix=r'cars', viewset=CarViewSet, basename='cars')
 
 urlpatterns = [
-                  path('motorcycle/create/', MotorcycleCreateAPIView.as_view(), name='motorcycle_create'),
-                  path('motorcycle/', MotorcycleListAPIView.as_view(), name='motorcycle_list'),
-                  path('motorcycle/<int:pk>/', MotorcycleListAPIView.as_view(), name='motorcycle_get'),
-                  path('motorcycle/update/<int:pk>/', MotorcycleUpdateAPIView.as_view(), name='motorcycle_update'),
-                  path('motorcycle/delete/<int:pk>/', MotorcycleDestroyAPIView.as_view(), name='motorcycle_delete'),
-              ] + router.urls
+    # Motorcycle
+    path('motorcycle/create/', MotorcycleCreateAPIView.as_view(), name='motorcycle_create'),
+    path('motorcycle/', MotorcycleListAPIView.as_view(), name='motorcycle_list'),
+    path('motorcycle/<int:pk>/', MotorcycleListAPIView.as_view(), name='motorcycle_get'),
+    path('motorcycle/update/<int:pk>/', MotorcycleUpdateAPIView.as_view(), name='motorcycle_update'),
+    path('motorcycle/delete/<int:pk>/', MotorcycleDestroyAPIView.as_view(), name='motorcycle_delete'),
+
+    # Mileage
+    path('mileage/create/', MileageCreateAPIView.as_view(), name='пробег'),
+
+] + router.urls
