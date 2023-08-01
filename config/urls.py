8761efffd1d6eee7462fe6aa6577b52ad785cb3d
DSulzhits <dsulzhits@gmail.com>
2023-08-01 12:41:54 +0300

    change url name

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('vehicle.urls', namespace='vehicle')),
+    path('api/', include('vehicle.urls', namespace='vehicle')),
 ]
