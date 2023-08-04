c4ad45b7bd99e4677deeb7220f934ad292ab8f57
DSulzhits <dsulzhits@gmail.com>
2023-08-04 17:24:16 +0300

    add new serializers: CarMileageSerializer MotorcycleMileageSerializer CarCreateSerializer

--- vehicle/serializers.py
+++ vehicle/serializers.py
@@ -9,14 +9,21 @@ class MileageSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class CarMileageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Mileage
+        fields = ['year', 'mileage']
+
+
 class CarSerializer(serializers.ModelSerializer):
     last_mileage = serializers.IntegerField(source='mileage_set.last.mileage', default=0, read_only=True)
-
     # mileage_set это queryset, т.е. набор данных;
     # last/first это либо последние данные, либо первые зависит от ordering в модели Mileage;
     # mileage обращение к полю модели mileage
     # default базовое значение
     # read_only если мы хотим эти данные генерировать только для выдачи
+
+    mileage = CarMileageSerializer(many=True, read_only=True, source='mileage_set')
 
     # для вывода всех пробегов реализуем переменную mileage куда передаем сериализатор CarMileageSerializer
     # many=True для вывода всех пробегов
@@ -40,3 +47,26 @@ class MotorcycleSerializer(serializers.ModelSerializer):
         if mileage:
             return mileage.mileage
         return 0
+
+
+class MotorcycleMileageSerializer(serializers.ModelSerializer):
+    motorcycle = MotorcycleSerializer(many=False)
+
+    class Meta:
+        model = Mileage
+        fields = ['year', 'mileage', 'motorcycle']
+
+
+class CarCreateSerializer(serializers.ModelSerializer):
+    mileage = CarMileageSerializer(many=True)
+
+    class Meta:
+        model = Car
+        fields = '__all__'
+
+    def create(self, validated_data):
+        mileage = validated_data.pop('mileage')
+        car_instance = Car.objects.create(**validated_data)
+        for m in mileage:
+            Mileage.objects.create(car=car_instance, **m)
+        return car_instance
