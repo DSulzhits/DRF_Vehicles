2f9ecfc6bd97b90705b74670f5bbd97386614b8d
DSulzhits <dsulzhits@gmail.com>
2023-08-07 11:11:15 +0300

    update config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('vehicle.urls', namespace='vehicle')),
+    path('users/', include('users.urls', namespace='users')),
 ]
